89f9dde70fa7742d184098ae629c27388903b9ad
sks11491 <sks11491@gmail.com>
2020-05-12 22:04:31 +0530

    commit for frontend

--- codedaddies_list/my_app/views.py
+++ codedaddies_list/my_app/views.py
@@ -1,7 +1,17 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 
 # Create your views here.
 
 def home(request):
     return render(request, "base.html")
+
+
+def new_search(request):
+    search_keyword = request.POST.get('search')
+    data_for_front = {
+        'search': search_keyword
+    }
+    return render(request, "my_app/new_search.html", data_for_front)
